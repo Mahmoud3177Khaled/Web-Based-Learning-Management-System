57ea258bf693a7eff34ebd198257a7fc4bd007b1
Mahmoud Khaled <124203317+Mahmoud3177Khaled@users.noreply.github.com>
2024-12-18 19:54:14 +0200

    added grades histroy in student

--- LMS/src/main/java/com/example/lms/entity/Student.java
+++ LMS/src/main/java/com/example/lms/entity/Student.java
@@ -2,6 +2,8 @@ package com.example.lms.entity;
 
 public class Student {
     private int id;
+    private int totalCorrectMarks;
+    private int totalQuizMarks;
 
     public int getId() {
         return id;
@@ -9,5 +11,21 @@ public class Student {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public void settotalCorrectMarks(int totalCorrectMarks) {
+        this.totalCorrectMarks = totalCorrectMarks;
+    }
+
+    public int gettotalCorrectMarks() {
+        return this.totalCorrectMarks;
+    }
+
+    public void setTotalQuizMarks(int totalQuizMarks) {
+        this.totalQuizMarks = totalQuizMarks;
+    }
+
+    public int getTotalQuizMarks() {
+        return this.totalQuizMarks;
     }
 }
