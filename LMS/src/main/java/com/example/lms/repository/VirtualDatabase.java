f19c8afcd611cf406b11db042ce4e54d73a017e7
Mahmoud Khaled <124203317+Mahmoud3177Khaled@users.noreply.github.com>
2024-12-18 19:49:37 +0200

    .

--- LMS/src/main/java/com/example/lms/repository/VirtualDatabase.java
+++ LMS/src/main/java/com/example/lms/repository/VirtualDatabase.java
@@ -6,13 +6,14 @@ import java.util.Map;
 
 import com.example.lms.entity.Course;
 import com.example.lms.entity.Question;
+import com.example.lms.entity.QuestionBank;
 import com.example.lms.entity.Quiz;
 import com.example.lms.entity.Student;
 
 public class VirtualDatabase {
     public static Map<Integer,Student> students = new HashMap<>();
     public static Map<String,Course> courses = new HashMap<>();
-    public static ArrayList<Question> banks = new ArrayList<>();
+    public static ArrayList<QuestionBank> banks = new ArrayList<>();
     public static ArrayList<Quiz> quizes = new ArrayList<>();
 
 }
