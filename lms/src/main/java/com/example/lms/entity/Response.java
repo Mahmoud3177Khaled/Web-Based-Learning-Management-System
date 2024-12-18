c52a328a4896494f656d75e8218e39f72941bbb1
philokaram <philokrm@gmail.com>
2024-12-18 19:09:35 +0200

    RoleAccessControl

---
+++ lms/src/main/java/com/example/lms/entity/Response.java
@@ -0,0 +1,21 @@
+package com.example.lms.entity;
+
+public class Response {
+    private int code;
+    private String message;
+    public Response(String message) {
+        this.message = message;
+    }
+    public int getCode() {
+        return code;
+    }
+    public void setCode(int code) {
+        this.code = code;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
