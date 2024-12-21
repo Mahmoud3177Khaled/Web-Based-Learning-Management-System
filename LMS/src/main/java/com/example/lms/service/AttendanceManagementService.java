b28fa7951a35458126f9cc24c249f3c24d5a2e5c
philokaram <philokrm@gmail.com>
2024-12-21 22:59:41 +0200

    Update AttendanceManagementService.java

--- LMS/src/main/java/com/example/lms/service/AttendanceManagementService.java
+++ LMS/src/main/java/com/example/lms/service/AttendanceManagementService.java
@@ -4,18 +4,26 @@ import org.springframework.stereotype.Service;
 
 import com.example.lms.entity.Course;
 import com.example.lms.entity.Lesson;
+import com.example.lms.entity.Notification;
+import com.example.lms.entity.Student;
 import com.example.lms.entity.OTPGenerator;
 import com.example.lms.repository.VirtualDatabase;
+import com.example.lms.service.NotificationService;
+
 
 @Service
 public class AttendanceManagementService {
 
+    private NotificationService notificationService;
     public boolean generateLessonOTP(String courseId,int lessonNumber){
         Course course = VirtualDatabase.courses.get(courseId);
         Lesson lesson =course.getLesson(lessonNumber);
         lesson.setOTPAttendanceCode(OTPGenerator.generateOTP());
         course.addLesson(lesson);
         VirtualDatabase.courses.put(courseId, course);
+        for (Student student : course.getEnrolledStudents().values()) {
+            notificationService.addCustomNotification(student.getId(), "the OTP code for Lesson "+lessonNumber+" for course "+course.getTittle()+" is: "+lesson.getOTPAttendanceCode());
+        }
         return true;
     }
     public boolean lessonAttends(String courseId,int lessonNumber,int studentId,String OTPCode){
