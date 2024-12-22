31bde5f8343adf1cbe655fd5e5b7aaea5775e457
Mahmoud Khaled <124203317+Mahmoud3177Khaled@users.noreply.github.com>
2024-12-22 22:53:47 +0200

    starting tests

---
+++ LMS/src/test/java/com/example/lms/QuestionBankServiceTests/AddQuestionToBankServiceTest.java
@@ -0,0 +1,12 @@
+package com.example.lms.QuestionBankServiceTests;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AddQuestionToBankServiceTest {
+
+    @Test
+	void submitQuestionTest() {
+        
+	}
+}
